f3b2c8b0c91eac0ead92737a84a8cf987bc30584
Joel K George <joelk9895@gmail.com>
2023-05-27 13:17:13 +0530

    Init

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import "./App.css";
+import Landing from "./Pages/Landing/Landing";
+
+function App() {
+  return (
+    <>
+      <Landing />
+    </>
+  );
+}
+
+export default App;
---
+++ src/Pages/Landing/Landing.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Landing = () => {
+  return (
+    <div>
+      <div className="app">
+        <h1>Calm</h1>
+        <h2>
+          Empowering Telemedicine, Real-time Health Data for Enhanced Diagnosis
+        </h2>
+      </div>
+    </div>
+  );
+};
+
+export default Landing;
