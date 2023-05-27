2f93f1ecb4f3fbf9cc2a8cbde64401439dd66127
Harikrishnan <harikrishnanb24@gmail.com>
2023-05-27 19:13:26 +0530

    landing page done

--- src/Pages/Landing/Landing.jsx
+++ src/Pages/Landing/Landing.jsx
@@ -1,14 +1,30 @@
 import React from "react";
+import "./Landing.css";
+
 
 const Landing = () => {
   return (
-    <div>
-      <div>
-        <h1>Calm</h1>
-        <h2>
-          Empowering Telemedicine, Real-time Health Data for Enhanced Diagnosis
-        </h2>
+    <div className="calm">
+      <div className="heading">
+        <h1>calm.</h1>
       </div>
+
+      <div className="subs">
+        <h3>Empowering Telemedicine,<br /> Real-time Health Data for Enhanced Diagnosis.</h3>
+      </div>
+      <div className="senti">
+        <p>
+          Where patient care meets cutting edge tecnology.
+        </p>
+      </div>
+      <div className="signin">
+        <button className="login-btn">
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRu1PJmT_THldF0n5APcmt9p10utgu6KSw4cH2fQ5Xhpw&s" alt="" />
+          <h3>Sign in with Google.</h3>
+          
+        </button>
+      </div>
+
     </div>
   );
 };
