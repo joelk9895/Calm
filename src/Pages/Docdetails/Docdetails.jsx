68d871498c540ecdc61018efcd0206d72362611f
Harikrishnan <harikrishnanb24@gmail.com>
2023-05-28 09:35:45 +0530

    hi

--- src/Pages/Docdetails/Docdetails.jsx
+++ src/Pages/Docdetails/Docdetails.jsx
@@ -10,18 +10,32 @@ const Docdetails = () => {
 
         <div className="form">
         <form action="" method="post">
+        <div className="wrap">
+            <div className="1">
             <label htmlFor="name">Name</label>
             <input type="text" name=""/>
+            </div>
+            <div className="2">
             <label htmlFor="experience">experience</label>
             <input type="text" experience=""/>
+            </div>
+            <div className="3">
             <label htmlFor="phone">phone</label>
             <input type="text" phone=""/>
+            </div>
+            <div className="4">
             <label htmlFor="">Specialization</label>
             <input type="text" name="Specialization"/>
+            </div>
+            <div className="5">
             <label htmlFor="">Medical License</label>
             <input type="File" name="Medical License"/>
+            </div>
+            <div className="6">
             <label htmlFor="">Upload Picture</label>
             <input type="File" name="Profile picture"/>
+            </div>
+        </div>        
 
         </form>
         </div>
