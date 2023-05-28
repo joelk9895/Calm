3d108821758a407fd57c22cd65aeb800df60d7a2
sanjay-sudheer <sanjaysudheer2003@gmail.com>
2023-05-28 09:48:55 +0530

    firetore

--- src/Pages/Docdetails/Docdetails.jsx
+++ src/Pages/Docdetails/Docdetails.jsx
@@ -1,34 +1,87 @@
-import React from "react";
-import "./Docdetails.css" 
+import React, { useState } from "react";
+import "./Docdetails.css";
+import { collection, addDoc } from "firebase/firestore";
+import { database } from "../firebase/firebase";
 
 const Docdetails = () => {
+  const [name, setName] = useState("");
+  const [experience, setExp] = useState("");
+  const [phone, setPhone] = useState("");
+  const [spec, setSpec] = useState(""); 
+  const [meet, setmeet] = useState('')
+
+  const sub = async (e) => {
+    e.preventDefault();
+
+    try {
+      // Add data to the "data" collection
+      await addDoc(collection(database, "data"), {
+        Name: name,
+        experience: experience,
+        phone: phone,
+        specialization: spec,
+        meet_link: meet
+      });
+
+      alert("Data Successfully Submitted");
+    } catch (error) {
+      console.error("Error adding document: ", error);
+    }
+  };
+
   return (
     <div className="docdetails">
-        <div className="detailstha">
-            <h3>Please Enter your Details.</h3>
-        </div>
+      <div className="detailstha">
+        <h3>Please Enter your Details.</h3>
+      </div>
 
-        <div className="form">
-        <form action="" method="post">
-            <label htmlFor="name">Name</label>
-            <input type="text" name=""/>
-            <label htmlFor="experience">experience</label>
-            <input type="text" experience=""/>
-            <label htmlFor="phone">phone</label>
-            <input type="text" phone=""/>
-            <label htmlFor="">Specialization</label>
-            <input type="text" name="Specialization"/>
-            <label htmlFor="">Medical License</label>
-            <input type="File" name="Medical License"/>
-            <label htmlFor="">Upload Picture</label>
-            <input type="File" name="Profile picture"/>
+      <div className="form">
+        <form action="" method="post" onSubmit={(event) => sub(event)}>
+          <label htmlFor="name">Name</label>
+          <input
+            type="text"
+            name=""
+            placeholder="your name"
+            onChange={(e) => setName(e.target.value)}
+          />
+          <label htmlFor="experience">Experience</label>
+          <input
+            type="text"
+            name="experience"
+            placeholder="experience"
+            onChange={(e) => setExp(e.target.value)}
+          />
+          <label htmlFor="phone">Phone</label>
+          <input
+            type="text"
+            name="phone"
+            placeholder="phone"
+            onChange={(e) => setPhone(e.target.value)}
+          />
+          <label htmlFor="">Specialization</label>
+          <input
+            type="text"
+            name="Specialization"
+            placeholder="specialization"
+            onChange={(e) => setSpec(e.target.value)}
+          />
+          <label htmlFor="">meet link</label>
+          <input
+            type="text"
+            name="meet"
+            placeholder="meet link"
+            onChange={(e) => setmeet(e.target.value)}
+          />
+          <label htmlFor="">Medical License</label>
+          <input type="File" name="Medical License" />
+          <label htmlFor="">Upload Picture</label>
+          <input type="File" name="Profile picture" />
 
+          <button type="submit">Submit</button>
         </form>
-        </div>
-
-
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Docdetails
+export default Docdetails;
